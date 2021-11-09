78a40631142d69f0596e20d6fc1f8f69acee9cf1
leomonay <leonardomonay@gmail.com>
2021-11-09 19:01:45 -0300

    removed mongoDB uri from code

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connection.on('open',()=>console.log('db connected'))
 
 async function connectDb (url){
-    // const uri = `mongodb://${host}:${port}/${dbName}`;
-    // const uri = `mongodb+srv://tecnoadmin:admin@cluster0.vwxyc.mongodb.net/tecnobase`;
     console.log(url);
     await mongoose.connect(url, {useNewUrlParser: true});
 }
